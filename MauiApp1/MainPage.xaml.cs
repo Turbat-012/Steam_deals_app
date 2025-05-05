473dee4a42ea7816f8361f0072c94609c8984e60
TurkhT <TurkhT@L306-04>
2025-05-05 13:06:44 +0800

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        //TODO: Add Navigation to home page for clicking on Steam icon 
+        private void NavigationToHomePage(object sender, NavigationEventArgs e)
+        {
+        }
+
+        //TODO: Add Hamburger menu and Profile navigation to the app
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ MauiApp1/WebsharkApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+//using Newtonsoft.Json;
+
+namespace MauiApp1
+{
+    internal class WebsharkApi
+    {
+        public async void GetAPI()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
